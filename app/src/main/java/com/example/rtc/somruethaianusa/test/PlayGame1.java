5d4c89e7d3b8c43888f881cf0d391c4d39dd0b1b
masterUNG <phrombutr@gmail.com>
2017-01-26 23:42:46 +0700

    จับเวลา หน้าแรกได้ละ

--- app/src/main/java/com/example/rtc/somruethaianusa/test/PlayGame1.java
+++ app/src/main/java/com/example/rtc/somruethaianusa/test/PlayGame1.java
@@ -2,15 +2,20 @@ package com.example.rtc.somruethaianusa.test;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PlayGame1 extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView imageView1, imageView2, imageView3;
     private int timeAnInt = 0;
+    private TextView textView;
+    private boolean aBoolean = true;
+    private int time = 120;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +25,39 @@ public class PlayGame1 extends AppCompatActivity implements View.OnClickListener
         imageView1 = (ImageView) findViewById(R.id.a);
         imageView2 = (ImageView) findViewById(R.id.b);
         imageView3 = (ImageView) findViewById(R.id.c);
+        textView = (TextView) findViewById(R.id.textView20);
 
         imageView1.setOnClickListener(this);
         imageView2.setOnClickListener(this);
         imageView3.setOnClickListener(this);
 
+        myLoop();
+
 
     }   // Main Method
+
+    private void myLoop() {
+
+        if (aBoolean) {
+
+            if (time == 0) {
+                aBoolean = false;
+                startActivity(new Intent(new Intent(PlayGame1.this, Gameover.class)));
+                finish();
+            }
+
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    time -= 1;
+                    textView.setText(Integer.toString(time) +  " Sec");
+                    myLoop();
+                }
+            }, 1000);
+        }
+
+    }   // loop
 
     @Override
     public void onClick(View view) {
